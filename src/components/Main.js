d538ebb1fd861c57cae3e3f7933f02eef7b7405a
Guannan Chen <guannanc996@gmail.com>
2019-09-26 23:47:58 -0700

    profile

---
+++ src/components/Main.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import nba from 'nba';
+
+import { Profile } from "./Profile";
+
+
+export class Main extends Component {
+    state = {
+        playerId: nba.findPlayer('Stephen Curry').playerId,
+        playerInfo: {},
+    }
+
+    componentDidMount() {
+        nba.stats.playerInfo({ PlayerID: this.state.playerId })
+            .then((info) => {
+                const playerInfo = Object.assign(info.commonPlayerInfo[0], info.playerHeadlineStats[0]);
+                console.log('playerInfo', playerInfo);
+                this.setState({ playerInfo });
+            })
+            .catch((e) => console.log(e))
+    }
+
+    render() {
+        return (
+            <div className="main">
+                <Profile playerInfo={ this.state.playerInfo} />
+            </div>
+        );
+    }
+}
+
